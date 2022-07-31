43042d9afcd71fbfbc79560e85b587a58e886c7c
Raven <108248559+unquestionablyraven@users.noreply.github.com>
2022-07-31 09:11:05 +0300

    Profile Page Update

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -20,41 +20,55 @@
             @csrf
             @method('UPDATE')
 
-            <span class="font-semibold text-lg text-gray-700">Your Information</span>
+            <span class="font-semibold text-lg text-gray-700">{{ __('misc.labels.headings.personal') }}</span>
 
             <div class="mt-4 flex items-center space-x-8">
                 <div class="w-1/2">
-                    <x-forms.label for="name" value="{{ __('Name') }}" />
+                    <x-forms.label for="name">{{ __('misc.labels.name') }}</x-forms.label>
 
-                    <x-forms.input id="name" class="block mt-1 w-full" type="text" name="name"
-                        value="{{ $user->name }}" required />
+                    <x-forms.input id="name" class="block mt-1 w-full" type="text" name="name" value="{{ $user->name }}" required />
                 </div>
 
                 <div class="w-1/2">
-                    <x-forms.label for="phone" value="{{ __('Phone Number') }}" />
+                    <x-forms.label for="phone">{{ __('misc.labels.phone') }}</x-forms.label>
 
-                    <x-forms.input id="phone" class="block mt-1 w-full" type="text" name="phone"
-                        value="{{ $user->profile->phone }}" required />
+                    <x-forms.input id="phone" class="block mt-1 w-full" type="text" name="phone" value="{{ $user->profile->phone }}" required />
                 </div>
             </div>
 
             <div class="mt-8 border-t border-gray-100"></div>
 
-            <span class="mt-8 font-semibold text-lg text-gray-700">Guradian's Information</span>
+            <span class="mt-8 font-semibold text-lg text-gray-700">{{ __('misc.labels.headings.emails') }}</span>
 
             <div class="mt-4 flex items-center space-x-8">
                 <div class="w-1/2">
-                    <x-forms.label for="guardian_email" value="{{ __('Guardian\'s Email') }}" />
+                    <x-forms.label for="azure_email">{{ __('misc.labels.emails.azure') }}</x-forms.label>
 
-                    <x-forms.input id="guardian_email" class="block mt-1 w-full" type="email" name="guardian_email"
-                        value="{{ $user->profile->guardian_email }}" required />
+                    <x-forms.input id="azure_email" class="block mt-1 w-full" type="email" name="azure_email" value="{{ $user->profile->azure_email }}" required :disabled="true" />
                 </div>
 
                 <div class="w-1/2">
-                    <x-forms.label for="guardian_phone" value="{{ __('Guardian\'s Phone Number') }}" />
+                    <x-forms.label for="current_email">{{ __('misc.labels.emails.personal') }}</x-forms.label>
 
-                    <x-forms.input id="guardian_phone" class="block mt-1 w-full" type="text" name="guardian_phone"
-                        value="{{ $user->profile->guardian_phone }}" required />
+                    <x-forms.input id="current_email" class="block mt-1 w-full" type="text" name="current_email" value="{{ $user->email }}" required />
+                </div>
+            </div>
+
+            <div class="mt-8 border-t border-gray-100"></div>
+
+            <span class="mt-8 font-semibold text-lg text-gray-700">{{ __('misc.labels.headings.guardian') }}</span>
+
+            <div class="mt-4 flex items-center space-x-8">
+                <div class="w-1/2">
+                    <x-forms.label for="guardian_email">{{ __('misc.labels.emails.guardian') }}</x-forms.label>
+
+                    <x-forms.input id="guardian_email" class="block mt-1 w-full" type="email" name="guardian_email" value="{{ $user->profile->guardian_email }}" required />
+                </div>
+
+                <div class="w-1/2">
+                    <x-forms.label for="guardian_phone">{{ __('misc.labels.phones.guardian') }}</x-forms.label>
+
+                    <x-forms.input id="guardian_phone" class="block mt-1 w-full" type="text" name="guardian_phone" value="{{ $user->profile->guardian_phone }}" required />
                 </div>
             </div>
         </form>
