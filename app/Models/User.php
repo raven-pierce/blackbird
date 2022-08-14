f2bf2ca2f76eecb6db14e8d8b5def61cabd72456
Raven <108248559+unquestionablyraven@users.noreply.github.com>
2022-08-14 17:39:50 -0400

    Environment Change and User Model Hidden Fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,6 +53,8 @@ class User extends Authenticatable implements MustVerifyEmail, FilamentUser
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_secret',
+        'two_factor_recovery_codes',
     ];
 
     /**
